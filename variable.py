dd47a438d5e7a69f6bc21e4c9d78df9edd0ae987
Khair-Un-Nisa05 <bintearif2004@gmail.com>
2024-07-11 14:03:00 -0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,15 @@
+height=3
+base=7
+f=0.5
+area=f*height*base
+print("area of a triangle",area)
+
+side=int(input("enter side:"))
+side=int(input("enter side:"))
+area= side*side
+print("area of a square",area)
+
+radius=float(input("enter first radius:"))
+radius=float(input("enter second radius:"))
+area=3.142*radius*radius
+print("area of a radius",area)
