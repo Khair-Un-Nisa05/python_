cd1cc707669edc53dbffd65f947131072f39a787
Khair-Un-Nisa05 <bintearif2004@gmail.com>
2024-07-22 11:15:06 -0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,12 @@
+def max_of_three(d, e, f):
+    if d >= e and d >= f:
+         print("d")
+    elif e >= d and e >= f:
+         print("e")
+    else:
+         print("f")
+
+max_of_three(4, 6, 8)  
+max_of_three(9, 6, 2)   
+max_of_three(4,6,2)    
+     
